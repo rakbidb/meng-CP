050fd0ef9257f7ed5d660f4ee0f89caeb92b1bce
rakbidb <rakbidb@gmail.com>
2023-02-11 19:54:46 +0700

    bismillah jadi imba

--- Codeforces/Java/Contest/1758A-SeeingDouble.py
+++
@@ -1,5 +0,0 @@
-t = int(input())
-for i in range(t):
-    stringInput = input()
-    reverseStringInput = stringInput[::-1]
-    print(stringInput + reverseStringInput)
--- Codeforces/Java/Contest/1760A-MediumNumber.py
+++
@@ -1,5 +0,0 @@
-import statistics
-t = int(input())
-for i in range(t):
-    n = map(int, input().split())
-    print(statistics.median(n))
